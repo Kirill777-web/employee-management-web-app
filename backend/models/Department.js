f2a140ac917e70126eade9cf943154ce03dc151b
Kirill777-web <ki.lazutin@gmail.com>
2024-02-26 19:41:41 -0400

    updated Department

--- backend/models/Department.js
+++ backend/models/Department.js
@@ -4,7 +4,7 @@ const departmentSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   manager: { type: String },
   description: { type: String }
-});
+}, { collection: 'departments' });
 
 const Department = mongoose.model('Department', departmentSchema);
 
