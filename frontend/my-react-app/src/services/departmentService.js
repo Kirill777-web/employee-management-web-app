4a4b51d96467e25835d366a60ab28e4a27a2f131
Kirill777-web <ki.lazutin@gmail.com>
2024-02-28 19:03:46 -0400

    changes in environment file

--- frontend/my-react-app/src/services/departmentService.js
+++ frontend/my-react-app/src/services/departmentService.js
@@ -1,5 +1,7 @@
-import process from 'process';
+
+/* eslint-env node */
 export const fetchDepartments = async () => {
+  console.log('API URL:', process.env.REACT_APP_API_URL);
   const response = await fetch(`${process.env.REACT_APP_API_URL}/api/departments`);
   if (!response.ok) {
     throw new Error('Failed to fetch departments');
