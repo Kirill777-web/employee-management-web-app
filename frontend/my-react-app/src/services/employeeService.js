98694a80af8f283f37c8f6e2ee34fb3fb2caf959
Kirill777-web <ki.lazutin@gmail.com>
2024-02-29 18:15:39 -0400

    changes in employeeService URL correction for fetching the right data

--- frontend/my-react-app/src/services/employeeService.js
+++ frontend/my-react-app/src/services/employeeService.js
@@ -1,7 +1,7 @@
 
 const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
 export const fetchEmployees = async () => {
-  const response = await fetch(`${apiUrl}/api/departments`);
+  const response = await fetch(`${apiUrl}/api/employees`);
   if (!response.ok) {
     throw new Error('Failed to fetch employees');
   }
